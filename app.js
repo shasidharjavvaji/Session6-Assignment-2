231845143381faad569a7a26816a72903e0f7eba
shasidharjavvaji <javvajishasidhar@gmail.com>
2016-09-21 15:58:54 +0530

    Session 6 Assignment 2

---
+++ app.js
@@ -0,0 +1,12 @@
+var num1 = 20;
+var num2 = 30;
+var num3 = 40;
+if (num1>num2){
+    console.log("Iam the Greater Number:"+num1);
+}
+else if (num2>num3) {
+    console.log("Iam the Greater Number:"+num2);
+}
+else {
+    console.log("Iam the Greater Number:"+num3)
+}
